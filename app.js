72b9485ce8e1b3792ebad18d7bda7f1da6cc7130
aditya <mail2adityasinghsln@gmail.com>
2024-05-29 14:16:46 +0530

    defined a method to generate the reponse based on primary contact id

--- app.js
+++ app.js
@@ -7,9 +7,33 @@ const { Op } = require('sequelize');
 const app = express();
 app.use(express.json());
 
+async function generateContactResponse(primaryContactId) {
+    // Retrieve the primary contact
+    const primaryContact = await Contact.findByPk(primaryContactId);
+
+    //Retrieve all secondary contacts linked to the primary contact
+    const secondaryContacts = await Contact.findAll({
+        where: {
+            linkedId: primaryContactId,
+            linkPrecedence: 'secondary'
+        }
+    });
+
+    // Create the response object
+    const response = {
+        contact: {
+            primaryContactId: primaryContact.id,
+            emails: [primaryContact.email, ...secondaryContacts.map(contact => contact.email).filter(email => email)],
+            phoneNumbers: [primaryContact.phoneNumber, ...secondaryContacts.map(contact => contact.phoneNumber).filter(phoneNumber => phoneNumber)],
+            secondaryContactIds: secondaryContacts.map(contact => contact.id)
+        }
+    };
+    return response;
+}
+
 app.post('/identify', async (req, res) => {
     const { email, phoneNumber } = req.body;
-
+    
     
 });
 
