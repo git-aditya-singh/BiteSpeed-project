d73cf439aafc3b3a2af9fb9d066776590fb46d66
aditya <mail2adityasinghsln@gmail.com>
2024-05-29 14:03:26 +0530

    Defined the Entity to store in Database

---
+++ models/contact.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Contact = sequelize.define('Contact', {
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    linkedId: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    linkPrecedence: {
+        type: DataTypes.ENUM('secondary', 'primary'),
+        defaultValue: 'primary'
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    timestamps: true,
+    paranoid: true
+});
+
+module.exports = Contact;
